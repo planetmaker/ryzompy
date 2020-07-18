1b17d29fc33278b6f7f5ac249b9b2455e6c39b26
planetmaker <i.vonborstel@tu-bs.de>
2020-07-18 15:43:34 +0200

    Change [weather]: recreate graph from BM website

--- weather.py
+++ weather.py
@@ -12,6 +12,9 @@ import requests
 import json
 import pandas as pd
 
+import matplotlib.pyplot as plt
+plt.close('all')
+
 
 apiurl = 'https://api.bmsite.net/atys/weather?cycles=8&offset=1'
 
@@ -32,15 +35,72 @@ translation_table = {
 data = requests.get(apiurl)
 weather_json = json.loads(data.text)
 
+def cycle_to_hour(c):
+    """
+
+    Parameters
+    ----------
+    c : int
+        cycles number
+
+    Returns
+    -------
+    int
+        hour of day of the current cycle
+
+    """
+    return c * 3
+
+def cycle_to_local_hour(c):
+    return (c - c // 8 * 8) * 3
+
+def time_of_day(current_hour):
+    """
+    Parameters
+    ----------
+    current_hour : float
+        current hour as returned by ballistic mystics.
+
+    Returns
+    -------
+    float
+        time of day as floating point value of the hour
+
+    """
+    return current_hour - (current_hour // 24) * 24
+
 weather = dict()
+current_cycle = int(weather_json['cycle'])
+ingame_time  = float(weather_json['hour'])
+
 for item,value in translation_table.items():
     tmp = pd.DataFrame.from_records(weather_json['continents'][item]).T
     weather[item] = tmp.astype({'value': 'float64','cycle':'int64'})
 
 yticks = [0, 0.167, 0.334, 0.500, 0.666, 0.834]
+weather['zorai']['hour'] = cycle_to_hour(weather['zorai']['cycle'])
+
+# ax = weather['zorai'].plot(x='hour', y='value', grid='True', title='Wettervorhersage', yticks=yticks)
+# ax.axvline(x=ingame_time/3,ymin=0,ymax=1)
 
 # Comparison to the ballistic mythics website:
 # There are added two flat hours, then an hour of change to the new value
 # The length of 8 weather cycles corresponds to the display on the website
-weather['zorai'].plot(x='cycle', y='value', grid='True', title='Wettervorhersage', yticks=yticks)
+# 58400 weather cycles per year
+print("Cycle, hour, local hour: ",current_cycle, ingame_time, time_of_day(ingame_time))
+
+
+iw = weather['zorai']
+iw.set_index('hour', inplace=True)
+iw2 = iw
+for index, row in iw.iterrows():
+    iw2.loc[index+1] = row
+    iw2.loc[index+2] = row
+iw2.sort_index(inplace=True)
+
+w2 = dict()
+w2['zorai'] = iw2
+
+ax = w2['zorai'].plot(y='value', grid='True', title='Wettervorhersage', yticks=yticks)
+ax.axvline(x=ingame_time,ymin=0,ymax=1)
 
