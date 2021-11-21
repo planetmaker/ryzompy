9de1bcf3e634f12b424d957a16a60ff1d02e6ae3
planetmaker <i.vonborstel@tu-bs.de>
2021-11-21 20:07:55 +0100

    [social] Add: License information and readme

---
+++ social/README.md
@@ -0,0 +1,4 @@
+# Social time analysis (STA)
+
+Social time analysis is a a python-based data analysis tool to allow
+visualize and allow checking datasets for correlations.
--- social/social_globals.py
+++ social/social_globals.py
@@ -6,5 +6,20 @@ Created on Sun Nov 21 19:05:45 2021
 @author: planetmaker
 """
 
+__license__ = """
+socialgraph is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+socialgraph is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with NML; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."""
+
 seconds_per_day = 24 * 60 * 60
 seconds_per_week = seconds_per_day * 7
