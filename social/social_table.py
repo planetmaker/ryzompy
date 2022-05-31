a159ae3f3bc6e5cae33b6e6e541164a564702f03
planetmaker <i.vonborstel@tu-bs.de>
2022-05-31 08:49:54 +0200

    Fix [social_table]: import config to actually use it. Add [social_table]: Allow reading all names and adding their data to the time_table if the number of datapoints exceeds a given amount

--- social/social_table.py
+++ social/social_table.py
@@ -10,6 +10,10 @@ import pandas as pd
 import datetime
 from social_API import Social_API
 from character import Character
+import matplotlib.pyplot as plt
+
+global config
+from social_config import config
 
 class Social_Table():
     """
@@ -35,9 +39,15 @@ class Social_Table():
             self.timeinfo = pd.DataFrame([])
         else:
             print("Reading from csv currently not implemented!\nName list not populated.")
-        self.filename = "social_table.pkl"
+        self.filenames = {
+            "changes": "social_table_changes.pkl",
+            "charinfo": "social_table_charinfo.pkl",
+            "timeinfo": "social_table_timeinfo.pkl",
+            }
 
-    def read_pickle(self, filename=""):
+
+
+    def read_pickle(self, filenames=None):
         """
         Read the changes Dataframe from a pickle file
 
@@ -51,16 +61,18 @@ class Social_Table():
         None.
 
         """
-        if filename == "":
+        if filenames == None:
             try:
-                filename = config["status_filename"]
+                filenames = config["social_filenames"]
             except:
-                filename = self.filename
-        self.changes = pd.read_pickle(filename)
+                filenames = self.filenames
+        self.changes = pd.read_pickle(filenames['changes'])
+        self.charinfo = pd.read_pickle(filenames['charinfo'])
+        self.timeinfo = pd.read_pickle(filenames['timeinfo'])
 
 
 
-    def save_pickle(self, filename=""):
+    def save_pickle(self, filenames=None):
         """
         Save the changes Dataframe to a pickle file
 
@@ -74,12 +86,16 @@ class Social_Table():
         None.
 
         """
-        if filename == "":
+        if filenames == None:
             try:
-                filename = config["status_filename"]
+                filenames = config["social_filenames"]
             except:
-                filename = self.filename
-        self.changes.to_pickle(filename)
+                filenames = self.filenames
+        self.changes.to_pickle(filenames['changes'])
+        self.charinfo.to_pickle(filenames['charinfo'])
+        self.timeinfo.to_pickle(filenames['timeinfo'])
+
+
 
     def get_name_table(self):
         """
@@ -92,6 +108,8 @@ class Social_Table():
         """
         return self.charinfo
 
+
+
     def get_changes_table(self):
         """
         Return the whole dataframe unabridged
@@ -103,6 +121,8 @@ class Social_Table():
         """
         return self.changes
 
+
+
     def get_time_table(self):
         """
         Return the time table
@@ -113,6 +133,8 @@ class Social_Table():
 
         """
         return self.timeinfo
+
+
 
     def api_download_name(self, name):
         """
@@ -142,23 +164,35 @@ class Social_Table():
         self.charinfo['num_entries'][name]  = len(name_data)
         try:
             self.charinfo['char'][name] = Character(name, df[['time','status']])
-            # Add data to global time table
-            df['status'].replace({'online':1, 'offline':0, '':pd.NA}, inplace=True)
-            df.rename(columns={'status': name}, inplace = True)
-            df['time'] = [datetime.datetime.fromtimestamp(t) for t in df['time']]
-            df.set_index('time', inplace=True)
-            print(df)
-            self.timeinfo = self.timeinfo.combine_first(df)
+            print("Added ",name," to character list.")
         except KeyError as e:
             print("Error adding char data for ", name)
             print(e)
             print(df)
-        # except:
-        #     print("Unknown error for ", name)
-        #     print(df)
+        except:
+            print("Unknown error for ", name)
+            print(df)
+
+        # Add data to global time table
+        if len(name_data) >= config['min_datapoints']:
+            try:
+                df['status'].replace({'online':1, 'offline':0, '':pd.NA}, inplace=True)
+                df.rename(columns={'status': name}, inplace = True)
+                df['time'] = [datetime.datetime.fromtimestamp(t) for t in df['time']]
+                df.set_index('time', inplace=True)
+                self.timeinfo = self.timeinfo.combine_first(df)
+                print("Added ",name," to time table.")
+            except KeyError as e:
+                print("Error adding char data for ", name)
+                print(e)
+                print(df)
+            except:
+                print("Unknown error for ", name)
+                print(df)
 
 
-    def api_download_names(self, names):
+
+    def api_download_names(self, names, alle=False):
         """
         Add the data by name(s) to the table
 
@@ -172,11 +206,58 @@ class Social_Table():
         None.
 
         """
-        if type(names) == 'str':
+        if alle:
+            names = list(self.charinfo.index)
+
+        if isinstance(names, str):
             self.api_download_name(names)
-        elif (type(names) == 'list' or type(names) == 'set'):
+        elif (isinstance(names, list) or isinstance(names, set)):
             for name in names:
                 self.api_download_name(name)
         else:
-            print("Unsupported type for 'names'!")
+            print("Unsupported type for 'names': ", type(names))
+
+
+
+    def sanitize_timetable(self):
+        """
+        Replace all NaN values by proper numbers
+
+        Returns
+        -------
+        None.
+
+        """
+        # There is an id column... that's spurious.
+        try:
+            self.timeinfo.drop('id', 1, inplace=True)
+        except KeyError:
+            pass
+        self.timeinfo.fillna(method='ffill', inplace=True)
+        self.timeinfo.fillna(value=0, inplace=True)
+
+
+
+    def get_character_count(self, do_plot=False):
+        """
+        Get the number of online players from the subset of those where data
+        are available
+
+        Parameters
+        ----------
+        do_plot : TYPE, optional
+            DESCRIPTION. The default is False.
+
+        Returns
+        -------
+        num : TYPE
+            DESCRIPTION.
+
+        """
+        num = self.timeinfo.sum(axis=1)
+        if do_plot:
+            plt.plot(num)
+        return num
+
+
 
